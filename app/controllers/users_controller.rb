4f2144f0003bb2c9de22a3224d747ff83ad5c269
Jasmine Reitano <jasminereitano7@gmail.com>
2019-05-28 12:33:37 -0500

    create show action, add gravatar image and user articles to user's page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,9 @@ class UsersController < ApplicationController
     end
   end
 
-
+  def show
+    @user = User.find(params[:id])
+  end
 
 
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user, options = {size:80})
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class:"pt-2 pb-2 mx-auto d-block rounded-circle")
+  end
 end
