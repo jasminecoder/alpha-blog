fb38318672f5fbe007c52290e264e6edc9a05728
Jasmine Reitano <jasminereitano7@gmail.com>
2019-05-31 12:41:53 -0500

    modify users def create to keep user logged in when signed up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
+      session[:user_id] = @user.id
       redirect_to articles_path
+    else
+      render 'new'
     end
   end
 
